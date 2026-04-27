c7fcd58d82f6216885145c08d6ee0cfbe3a9840f
Mehrnaz Khorrami <mehrnazkhorrami769@gmail.com>
2026-04-27 23:46:36 +0330

    build: form card component and style

---
+++ src/components/form/FormCard.jsx
@@ -0,0 +1,15 @@
+
+import "./FormCard.css";
+
+function FormCard() {
+    return (
+      <>
+      <div className="card-container mr-6 pa-5">
+        <div className="dark-purple-color font-weight-800 fs-17">Add Entry</div>
+      </div>
+      </>
+    )
+  }
+  
+  export default FormCard
+  
