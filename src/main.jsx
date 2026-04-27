285daf2c55eb9cc36cf2706bc51c6f96257e5e1d
Mehrnaz Khorrami <mehrnazkhorrami769@gmail.com>
2026-04-27 23:44:57 +0330

    feat: import global css files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/color.css'
+import './styles/display.css'
+import './styles/typography.css'
+import './styles/variables.css'
+import './styles/reset.css'
+import './styles/size.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
