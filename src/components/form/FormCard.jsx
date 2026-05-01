caccc09fe381aea09fa97efc875118d58d83ecd2
Mehrnaz Khorrami <mehrnazkhorrami769@gmail.com>
2026-05-01 19:36:06 +0330

    feat: inputs and labels

--- src/components/form/FormCard.jsx
+++ src/components/form/FormCard.jsx
@@ -4,8 +4,25 @@ import "./FormCard.css";
 function FormCard() {
     return (
       <>
-      <div className="card-container mr-6 pa-5">
-        <div className="dark-purple-color font-weight-800 fs-17">Add Entry</div>
+      <div className="card-container mr-6 pa-7">
+        <div className="dark-purple-color font-weight-800 fs-17 mb-5 mt-2">Add Entry</div>
+
+        <div>
+          <label for="date" className="fs-14 input-label">Date</label>
+          <input id="date" type="text" name="date" className="input-field mt-2"/>
+        </div>
+        <div className="mt-4 mb-4">
+          <label for="language" className="fs-14 input-label">Language</label>
+          <input id="language" type="text" name="language" className="input-field mt-2"/>
+        </div>
+        <div>
+          <label for="card" className="fs-14 input-label">Card</label>
+          <input id="card" type="text" name="card" className="input-field mt-2"/>
+        </div>
+
+        <button className="generate-button mt-6">
+          <span className="white-color font-weight-800 fs-16">Create Change Log</span>
+        </button>
       </div>
       </>
     )
