d1788f0fb807dadf7a5fe61dbcc0d52991f15dc5
Mehrnaz Khorrami <mehrnazkhorrami769@gmail.com>
2026-05-08 10:18:10 +0330

    feat: handel copy logic

--- src/components/generated-code/GeneratedCodeCard.jsx
+++ src/components/generated-code/GeneratedCodeCard.jsx
@@ -1,26 +1,37 @@
+import { useState } from "react";
 import "./GeneratedCodeCard.css";
 
-function GeneratedCodeCard({changeLogData}) {
-  return (
-    
-      <div className="card-container generated-code-card-container pa-7">
-        <div className="d-flex align-items-center justify-space-between">
-          <div className="dark-purple-color font-weight-800 fs-17">Generated JSON</div>
-          <div>
-            <button className="copy-button">
-              <div className="d-flex align-items-center justify-space-between">
-                <img src="/src/assets/icons/copy.svg" width={16} height={16} />
-                <span className="font-weight-800 white-color mt-1">Copy</span>
-              </div>
-            </button>
-          </div>
-        </div>
+function GeneratedCodeCard({ changeLogData }) {
 
-        <pre className="json-generated-container mt-4 pa-5">
-          {changeLogData ? JSON.stringify(changeLogData, null, 2) : '{ }'}
-        </pre>
+  const [copy, setCopy] = useState(false);
+  const copyText = (text) => {
+    navigator.clipboard.writeText(text).then(() => {
+      setCopy(true);
+    })
+  };
+
+  return (
+
+    <div className="card-container generated-code-card-container pa-7">
+      <div className="d-flex align-items-center justify-space-between">
+        <div className="dark-purple-color font-weight-800 fs-17">Generated JSON</div>
+        <div>
+          <button className="copy-button" onClick={() => copyText(changeLogData?.data ? JSON.stringify(changeLogData.data, null, 2) : '{ }')}>
+            <div className="d-flex align-items-center justify-center">
+              <img src="/src/assets/icons/copy.svg" width={16} height={16} className="mr-2"/>
+              <span className="font-weight-800 white-color mt-1">
+                {copy ? "Copied" : "Copy"}
+              </span>
+            </div>
+          </button>
+        </div>
       </div>
-   
+
+      <pre className="json-generated-container mt-4 pa-5">
+        {changeLogData?.data ? JSON.stringify(changeLogData.data, null, 2) : '{ }'}
+      </pre>
+    </div>
+
   )
 }
 
